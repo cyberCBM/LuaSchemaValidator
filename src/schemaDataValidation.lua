f87e92043d43c341a762f7464c3ccbe78d4ea224
cybercbm <cyber.cbm@gmail.com>
2019-11-07 21:44:27 +0530

    1) validator namespace(table) added so that it can expose required API 2) error creator function added 3) validatefromfile function added 4) load data and schema from file function added

--- src/schemaDataValidation.lua
+++ src/schemaDataValidation.lua
@@ -19,3 +19,73 @@
 -- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 -- SOFTWARE.
+
+-- @Scope validator
+validator = {}
+
+-- Import from global environment and make local for easy usage.
+local type = type
+local pairs = pairs
+local format = string.format
+local insert = table.insert
+local next = next
+
+local function loadTableFromFile(fileName)
+    return true
+end
+
+--- Generate error message for validators.
+--
+-- @param data mixed
+--   Value that failed validator.
+-- @param expected_type string
+--   Expected type for data
+--
+-- @return
+--   String describing the error.
+---
+local function error_message(data, expected_type)
+    if data then
+        return format('%s is not %s.',tostring(data), expected_type)
+    end
+
+    return format(' is missing and should be %s.', expected_type)
+end
+
+
+-- One of the main validator function 
+--
+-- @param dataTable Table
+--
+-- @param schemaTable Table
+-- @return
+--   result and Table 
+--   This validator return value is either true on success or false and
+--   a nested table holding all errors.
+
+function validator.validateFromFile(dataTableFile, schemaTableFile)
+    local result, err = nil
+    local dataTable = nil
+    local errorTable = {}
+    
+    -- load table data
+    result, err, dataTable = loadTableFromFile(dataTableFile)
+    if result == false then
+        insert(errorTable, "Not valid data file-" .. err)
+        return false, errorTable
+    end
+    
+    -- load schema data
+    local schemaTable = nil
+    result, err, schemaTable = loadTableFromFile(schemaTableFile)
+    if result == false then
+        insert(errorTable, "Not valid schema file-" .. err)
+        return false, errorTable
+    end
+    
+    -- as both table loaded start validator
+    --print("dataTable:", dataTable)
+    --print("schemaTable:", schemaTable)  
+    return validator.validateAgainstSchema(dataTable, schemaTable)
+end
+
